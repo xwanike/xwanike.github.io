f238db67d8ae9e3289449759578c041cfd615f82
XwanikE <xwanike5006@gmail.com>
2023-10-10 16:43:47 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Togle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//Ketika hamburger diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klik di luar side bar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
