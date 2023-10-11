df740ef4f11faee1fd1b1da025cac4ffbb343a76
XwanikE <xwanike5006@gmail.com>
2023-10-11 20:04:35 +0800

    pembaruan card

--- js/script.js
+++ js/script.js
@@ -13,3 +13,40 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+var tombolBacaSelengkapnya = document.querySelectorAll(".baca-selengkapnya");
+
+// Menggunakan forEach untuk menambahkan event listener ke setiap tombol
+tombolBacaSelengkapnya.forEach(function (tombol) {
+  tombol.addEventListener("click", function () {
+    var kontenTambahan = this.nextElementSibling; // Menggunakan this untuk merujuk ke tombol yang ditekan
+    if (
+      kontenTambahan.style.display === "none" ||
+      kontenTambahan.style.display === ""
+    ) {
+      kontenTambahan.style.display = "block";
+      this.textContent = "Sembunyikan";
+    } else {
+      kontenTambahan.style.display = "none";
+      this.textContent = "Baca Selengkapnya";
+    }
+  });
+});
+
+feather.replace();
+
+const menuCardImages = document.querySelectorAll(".menu-card-img");
+const menuCardContainer = document.querySelector(".menu-card-img-container");
+
+let currentImageIndex = 0;
+
+menuCardContainer.addEventListener("click", () => {
+  currentImageIndex = (currentImageIndex + 1) % menuCardImages.length;
+  menuCardImages.forEach((image, index) => {
+    if (index === currentImageIndex) {
+      image.classList.add("enlarged");
+    } else {
+      image.classList.remove("enlarged");
+    }
+  });
+});
